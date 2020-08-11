8043066e99d9e031a7391462019592ba56f187fd
unni12345 <rajkrishnanv@gmail.com>
2020-08-11 14:24:15 +0530

    Feature:User added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
+  devise_for :users
   #
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
-  root "posts#index"
+  # root "posts#index"
 end
